23a833a71564a0aea24458b355764cf07048604d
Chamara Indrajith <chamara.indrajith@outlook.com>
2022-09-15 15:07:05 +0530

    endpoints

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,25 @@
 
 <h3>Set News Data</h3>
 
-<a href="api/set/news/soccer/date/{{$today}}">api/set/news/soccer/date/{{$today}}</a> <br>
-<a href="api/set/news/cricket/date/{{$today}}">api/set/news/cricket/date/{{$today}}</a>
+<a target="_blank" href="api/set/news/soccer/date/{{$today}}">api/set/news/soccer/date/{{$today}}</a> <br>
+<a target="_blank" href="api/set/news/cricket/date/{{$today}}">api/set/news/cricket/date/{{$today}}</a> <br>
+<a target="_blank" href="api/set/news/basketball/date/{{$today}}">api/set/news/basketball/date/{{$today}}</a>
+
+<h3>Set Sports Data</h3>
+
+<a target="_blank" href="api/set/soccer/date/{{$today}}">api/set/soccer/date/{{$today}}</a> <br>
+<a target="_blank" href="api/set/cricket/date/{{$today}}">api/set/cricket/date/{{$today}}</a> <br>
+<a target="_blank" href="api/set/basketball/date/{{$today}}">api/set/basketball/date/{{$today}}</a>
+
+<h3>Get Sports Data</h3>
+
+<a target="_blank" href="api/get/soccer/date/{{$today}}">api/get/soccer/date/{{$today}}</a> <br>
+<a target="_blank" href="api/get/cricket/date/{{$today}}">api/get/cricket/date/{{$today}}</a> <br>
+<a target="_blank" href="api/get/basketball/date/{{$today}}">api/get/basketball/date/{{$today}}</a>
+
+<h3>Get Sports Data (JSON)</h3>
+
+<a target="_blank" href="api/get/soccer/date/{{$today}}">api/get/soccer/date/{{$today}}/json</a> <br>
+<a target="_blank" href="api/get/cricket/date/{{$today}}">api/get/cricket/date/{{$today}}/json</a> <br>
+<a target="_blank" href="api/get/basketball/date/{{$today}}">api/get/basketball/date/{{$today}}/json</a>
+
