d3007d0a1491cae3b0c3880db1f331ca58c6a3d9
chamaraindrajith <chamara.indrajith@outlook.com>
2023-10-03 13:53:58 +0530

    getData()

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\getData
+use App\Http\Controllers\getData;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/app/date/{sport}/{date}/{utc_offset}' , [getData::class, 'app']);
+Route::get('/app/date/{sport}/{date}/{utc_offset}', [getData::class, 'app']);
